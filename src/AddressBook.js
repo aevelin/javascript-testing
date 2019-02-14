1878aa60fd3f7b5f5fe084c1e5edbf0b1ab38ace
Evelin Arust <evelin.arust@khk.ee>
2019-02-14 12:03:57 +0200

    Compleating Implementation

--- src/AddressBook.js
+++ src/AddressBook.js
@@ -1,4 +1,16 @@
 //Konstruktor-funktsioon AddressBook
-function AddressBook () {
+function AddressBook() {
+    //Uue omaduse lisamine. Tegemist on massiiviga
+    this.contacts = [];
+}
 
+//Uue funktsiooni addContact lisamine, mis aktsepteerib contacti kui parameetrit
+//ning lükkab selle kontakti massiivi
+AddressBook.prototype.addContact = function (contact) {
+    this.contacts.push(contact);
+}
+
+//Uus funktsioon, mis aktsepteerib indeksit ning tagastab massiivi elemendi selle indeksiga
+AddressBook.prototype.getContact = function (index) {
+    return this.contacts[index];
 }
